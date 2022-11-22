e60924062e7921258132d658da6cfa81ac50899c
YarinBS <yarin10121@gmail.com>
2022-11-22 12:12:54 +0200

    viterbi changes

--- inference.py
+++ inference.py
@@ -1,127 +1,83 @@
-from tqdm import tqdm
 import numpy as np
-from itertools import product
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 from preprocessing import read_test
+
+from itertools import product
 from collections import OrderedDict
 from preprocessing import represent_input_with_features
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
 
 
-def build_history(sentence, k, pp_tag, p_tag, c_tag):
-    c_word = sentence[k]
-    p_word = sentence[k - 1]
-    pp_word = sentence[k - 2]
-    n_word = sentence[k + 1]
-
-    h = (
-        c_word,
-        c_tag,
-        p_word,
-        p_tag,
-        pp_word,
-        pp_tag,
-        n_word
-    )
-    return h
-
-
-def calc_exp_val(pre_trained_weights, h, feature2id):
-    features = represent_input_with_features(h, feature2id.feature_to_idx)
-    return np.exp(np.sum(pre_trained_weights[features]))
-
-
-def last_tags_pair(pi_dict):
-    max_val = -1
-    max_pair = None
-    for pair, value in pi_dict.items():
-        if value > max_val:
-            max_val = value
-            max_pair = pair
-    return max_pair
+def get_hist(sentence, k, pp_tag, p_tag, c_tag):
+    """
+    Extracts a history vector for a given index in a sentence
+    """
+    return (sentence[k], c_tag, sentence[k - 1], p_tag, sentence[k - 2], pp_tag, sentence[k + 1])
 
 
 def memm_viterbi(sentence, pre_trained_weights, feature2id):
-    """
-    Write your MEMM Viterbi implementation below
-    You can implement Beam Search to improve runtime
-    Implement q efficiently (refer to conditional probability definition in MEMM slides)
-    """
-    n = len(sentence)
     tags = list(feature2id.feature_statistics.tags.copy())
     tag_pairs = list(product(tags, repeat=2))
-
-    # Initial pi tables
-    pi_dicts = {1: {i: 0 for i in tag_pairs}}
-    pi_dicts[1][('*', '*')] = 1
-
-    # initial bp tables
-    bp_dicts = {}
-
-    # initial q
+    big_pi_table = {1: {i: 0 for i in tag_pairs}}
+    big_pi_table[1][('*', '*')] = 1
+    backpointer_table = {}
     q = OrderedDict()
 
-    for k in range(2, n - 1):
-        pi_dicts[k] = OrderedDict()
-        bp_dicts[k] = OrderedDict()
+    for k in range(2, len(sentence) - 1):
+        big_pi_table[k], backpointer_table[k] = OrderedDict(), OrderedDict()
 
         if k == 2:
-            s_minus_1 = ['*']
-            s_minus_2 = ['*']
-            s = tags
-
+            s_1, s_2, s = ["*"], ["*"], tags
         elif k == 3:
-            s_minus_1 = tags
-            s_minus_2 = ['*']
-            s = tags
-
+            s_1, s_2, s = tags, ["*"], tags
         else:
-            s_minus_1 = tags
-            s_minus_2 = tags
-            s = tags
+            s_1, s_2, s = tags, tags, tags
 
-        for u, v in product(s_minus_1, s):
+        for u, v in product(s_1, s):
+            max_prob, best_tag = -1, None
 
-            # future elements to place in pi(k, u, v)
-            max_probability = -1
-            best_tag = None
+            # ---- Beam Search ----
+            beam_width = 5
+            if len(s_2) >= beam_width:
+                beam_probs = [big_pi_table[k - 1][t, u] for t in s_2]
+                beam_probs_indexes = np.argpartition(beam_probs, kth=(len(beam_probs) - beam_width))[-beam_width:]
+                s_2 = np.array(s_2)[beam_probs_indexes]
+            # ---------------------
 
-            width = 5
-            if len(s_minus_2) >= 5:
-                beam_probs = [pi_dicts[k - 1][t, u] for t in s_minus_2]
-                beam_probs_indexes = np.argpartition(beam_probs, kth=(len(beam_probs) - width))[-width:]
-                s_minus_2 = np.array(s_minus_2)[beam_probs_indexes]
-
-            # searching for the max and argmax
-            for t in s_minus_2:
-
-                h = build_history(sentence=sentence, k=k, pp_tag=t, p_tag=u, c_tag=v)
+            for t in s_2:
+                h = get_hist(sentence, k, t, u, v)
                 if h not in q:
                     softmax_denominator = 0
-                    for sm_t in tags:
-                        curr_h = build_history(sentence=sentence, k=k, pp_tag=t, p_tag=u, c_tag=sm_t)
-                        sm_t_exp_val = calc_exp_val(pre_trained_weights, curr_h, feature2id)
-                        softmax_denominator += sm_t_exp_val
-                        q[curr_h] = sm_t_exp_val
+                    for tag in tags:
+                        curr_h = get_hist(sentence, k, t, u, tag)
+                        tag_value = np.exp(np.sum(
+                            pre_trained_weights[represent_input_with_features(curr_h, feature2id.feature_to_idx)]))
+                        softmax_denominator += tag_value
+                        q[curr_h] = tag_value
 
-                    for sm_t in tags:
-                        curr_h = build_history(sentence=sentence, k=k, pp_tag=t, p_tag=u, c_tag=sm_t)
+                    for tag in tags:
+                        curr_h = get_hist(sentence, k, t, u, tag)
                         q[curr_h] /= softmax_denominator
 
-                prob_t_val = pi_dicts[k - 1][t, u] * q[h]
-                if prob_t_val > max_probability:
-                    max_probability = prob_t_val
+                prob_t_val = big_pi_table[k - 1][t, u] * q[h]
+                if prob_t_val > max_prob:
+                    max_prob = prob_t_val
                     best_tag = t
 
-            # update k, u, v table
-            pi_dicts[k][u, v] = max_probability
-            bp_dicts[k][u, v] = best_tag
+            big_pi_table[k][u, v] = max_prob
+            backpointer_table[k][u, v] = best_tag
 
-    tag_n_minus_1, tag_n = last_tags_pair(pi_dict=pi_dicts[n - 2])
+    top_value, best_pair = -1, None
+    for p, v in big_pi_table[len(sentence) - 2].items():
+        if v > top_value:
+            top_value, best_pair = v, p
+    tag_n_minus_1, tag_n = best_pair
     pred = [tag_n, tag_n_minus_1]
 
-    for k in reversed(range(2, n - 3)):
-        next_tag = (bp_dicts[k + 2])[pred[-1], pred[-2]]
+    for k in reversed(range(2, len(sentence) - 3)):
+        next_tag = (backpointer_table[k + 2])[pred[-1], pred[-2]]
         pred.append(next_tag)
 
     return ["*"] + list(reversed(pred))
@@ -191,11 +147,10 @@ def tag_all_test(test_path, pre_trained_weights, feature2id, predictions_path):
 
     print(f'Accuracy for {total_words} words is: {hit_words / total_words:.3f}')
 
-    if tagged:
-        # confusion matrix
-        cm = confusion_matrix(y_true, y_pred, labels=labels)
+    if tagged:  # Only enter if we go over a tagged file
+        conf_mat = confusion_matrix(y_true, y_pred, labels=labels)
 
-        display = ConfusionMatrixDisplay(cm, display_labels=labels)
+        display = ConfusionMatrixDisplay(conf_mat, display_labels=labels)
         display.plot()
         plt.savefig(f'{file_name}_full_confusion_matrix.png')
 
@@ -211,7 +166,7 @@ def tag_all_test(test_path, pre_trained_weights, feature2id, predictions_path):
         top10_keys = sorted_keys[:min(len(sorted_keys), 10)]
         top10_idx = [i for i, label in enumerate(labels) if label in top10_keys]
         print(top10_keys)
-        top_10_cm = cm[top10_idx][:, top10_idx]
+        top_10_cm = conf_mat[top10_idx][:, top10_idx]
 
         display = ConfusionMatrixDisplay(top_10_cm, display_labels=top10_keys)
         display.plot()
